c23f14bfc8d0760113dfd998ae2d09e52e817ee2
lakshmisha123 <120448644+lakshmisha123@users.noreply.github.com>
2025-04-08 00:07:43 +0530

    updategitCheckout

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,3 +1,8 @@
 def call() {
     echo "✅ This is gitCheckout() from the shared library"
+    checkout([$class: 'GitSCM',
+               branches: [[name: "${git_branch}"]],
+               userRemoteConfigs: [[credentialsId: "${git_credentails}",
+               url: "${git_url}"]]])
+     echo "Git checkout to ${git_url} is success"
 }
